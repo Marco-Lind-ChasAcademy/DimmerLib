23fe56adac46cc362d17222fbe95f780bfdf338b
Marco-Lind-ChasAcademy <marco.lind@chasacademy.se>
2025-04-03 22:40:09 +0200

    Achieved great improvements on the anti-flickering, but discovered led_value issue

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,10 @@
 
 const uint8_t SENSOR = A4;
 const uint8_t LED = 3;
-const uint8_t DELAY_TIME = 100;
-const uint8_t AVERAGES = 5;
-const uint8_t PART_DELAY = DELAY_TIME / AVERAGES;
+const uint8_t POLLING_RATE = 100;
+const uint8_t AVERAGES = 10;
+const uint8_t PART_DELAY = 5;
+const uint8_t DELAY_TIME = POLLING_RATE - AVERAGES * PART_DELAY;
 
 uint32_t sensor_value_sum;
 uint16_t sensor_value_average;
@@ -14,6 +15,7 @@ void setup()
 {
   pinMode(SENSOR, INPUT);
   pinMode(LED, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop()
@@ -30,5 +32,8 @@ void loop()
   sensor_value_average = sensor_value_sum / AVERAGES;
   
   led_value = map(sensor_value_average, 0, 16383, 0, 255);
+  Serial.println(led_value);
   analogWrite(LED, led_value);
+
+  delay(DELAY_TIME);
 }
