b8667a330eea05e43c74e8eb75abace7cbcffc8a
Marco-Lind-ChasAcademy <marco.lind@chasacademy.se>
2025-04-04 00:34:48 +0200

    Working nicely now, following the inverse square law

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,13 @@ const uint8_t DELAY_TIME = POLLING_RATE - AVERAGES * PART_DELAY;
 uint32_t sensor_value_sum;
 uint16_t sensor_value_average;
 uint8_t led_value;
+float k = 2;  // You can adjust this value to make the transition sharper or smoother
 
 void setup()
 {
+  ledcSetup(0, 490, 8);
+  ledcAttachPin(LED, 0);
   pinMode(SENSOR, INPUT);
-  pinMode(LED, OUTPUT);
   Serial.begin(115200);
 }
 
@@ -27,13 +29,17 @@ void loop()
     sensor_value_sum += analogRead(SENSOR);
     delay(PART_DELAY);
   }
-  
-
   sensor_value_average = sensor_value_sum / AVERAGES;
   
-  led_value = map(sensor_value_average, 0, 16383, 0, 255);
-  Serial.println(led_value);
-  analogWrite(LED, led_value);
+  
+  led_value = 255 * pow((float)(sensor_value_average) / 4096, k);
+
+  Serial.print(led_value);
+  Serial.print(" ");
+  Serial.print(sensor_value_average);
+  Serial.println();
+  ledcWrite(0, led_value);
+
 
   delay(DELAY_TIME);
 }
