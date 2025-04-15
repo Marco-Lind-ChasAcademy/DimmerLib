63c2c7ebd3aeb3c172ef73ac1657d98e26742f2c
Marco-Lind-ChasAcademy <marco.lind@chasacademy.se>
2025-04-15 14:18:59 +0200

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,9 @@ DimmerLib::LightSensingDimmer dimmer_4(A4, 21, 20, A0, 3);
 
 // Example with an ISR for dimmer_1
 MAKE_MODE_SWITCH_ISR(dimmer_1_ISR, dimmer_1)
+MAKE_MODE_SWITCH_ISR(dimmer_2_ISR, dimmer_2)
+MAKE_MODE_SWITCH_ISR(dimmer_3_ISR, dimmer_3)
+MAKE_MODE_SWITCH_ISR(dimmer_4_ISR, dimmer_4)
 
 
 
@@ -20,6 +23,9 @@ void setup()
   DimmerLib::semInit(); // Initializing the pin semaphores
 
   attachInterrupt(digitalPinToInterrupt(dimmer_1.MODE_BUTTON_PIN), dimmer_1_ISR, RISING);
+  attachInterrupt(digitalPinToInterrupt(dimmer_2.MODE_BUTTON_PIN), dimmer_2_ISR, RISING);
+  attachInterrupt(digitalPinToInterrupt(dimmer_3.MODE_BUTTON_PIN), dimmer_3_ISR, RISING);
+  attachInterrupt(digitalPinToInterrupt(dimmer_4.MODE_BUTTON_PIN), dimmer_4_ISR, RISING);
 
   xTaskCreate(DimmerLib::runDimmerTask, "Dimmer 1", 1024, &dimmer_1, configMAX_PRIORITIES - 1, NULL);
   xTaskCreate(DimmerLib::runDimmerTask, "Dimmer 2", 1024, &dimmer_2, configMAX_PRIORITIES - 1, NULL);
